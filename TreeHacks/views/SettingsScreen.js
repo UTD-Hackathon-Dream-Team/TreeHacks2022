0cbf0d9db5f4901fa8536d9c8f5dc3577fd8f82d
meganjtran <meganjtran@gmail.com>
2022-02-20 09:40:08 -0600

    colors
    everything is gray

--- TreeHacks/views/SettingsScreen.js
+++ TreeHacks/views/SettingsScreen.js
@@ -10,12 +10,12 @@ export default function SettingsScreen(props) {
       return <View>
         <Heading style={{marginVertical: 10, textAlign: "center"}}><Text>This Weeks Streaks</Text></Heading>
         <HStack space={3} justifyContent="center">
-        <Center h="20" w="10" bg="#6ceb8e" rounded="md" shadow={2}>Mon</Center>
-        <Center h="20" w="10" bg="#ed6878" rounded="md" shadow={2}>Tue</Center>
-        <Center h="20" w="10" bg="#6ceb8e" rounded="md" shadow={2}>Wed</Center>
-        <Center h="20" w="10" bg="#6ceb8e" rounded="md" shadow={2}>Thu</Center>
-        <Center h="20" w="10" bg="#6ceb8e" rounded="md" shadow={2}>Fri</Center>
-        <Center h="20" w="10" bg="#ed6878" rounded="md" shadow={2}>Sat</Center>
+        <Center h="20" w="10" bg="#9cdc98" rounded="md" shadow={2}>Mon</Center>
+        <Center h="20" w="10" bg="#f7b1b1" rounded="md" shadow={2}>Tue</Center>
+        <Center h="20" w="10" bg="#9cdc98" rounded="md" shadow={2}>Wed</Center>
+        <Center h="20" w="10" bg="#9cdc98" rounded="md" shadow={2}>Thu</Center>
+        <Center h="20" w="10" bg="#9cdc98" rounded="md" shadow={2}>Fri</Center>
+        <Center h="20" w="10" bg="#f7b1b1" rounded="md" shadow={2}>Sat</Center>
         <Center h="20" w="10" bg={submit?"#6ceb8e":"#c6cbcc"} rounded="md" shadow={2}>Sun</Center>
       </HStack></View>;
   };
