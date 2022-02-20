a9c16a80eb4c7a03450e888e1038af26b849d36a
SaloniSS <sxs170015@utdallas.edu>
2022-02-20 01:29:34 -0600

    Calendar boi

--- TreeHacks/App.js
+++ TreeHacks/App.js
@@ -1,14 +1,15 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginScreen from "./views/LoginScreen";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import Calendar from "./views/Calendar";
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
     </View>
   );
@@ -20,11 +21,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen
-                name="LogIn"
-                options={{ headerShown: false }}
-                component={LoginScreen}
-              />
+        <Stack.Screen
+          name="LogIn"
+          options={{ headerShown: false }}
+          component={Calendar}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ TreeHacks/views/Calendar.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import { View, Text, Button, Platform } from "react-native";
+import * as Calendar from "expo-calendar";
+
+export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Calendar.requestCalendarPermissionsAsync();
+      if (status === "granted") {
+        console.log("Permission granted");
+      }
+    })();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Create a new calendar" onPress={createCalendar} />
+      <Button title="Create a Break" onPress={createEvent} />
+    </View>
+  );
+}
+
+async function getDefaultCalendarSource() {
+  const defaultCalendar = await Calendar.getDefaultCalendarAsync();
+  return defaultCalendar.source;
+}
+
+async function createCalendar() {
+  const defaultCalendarSource =
+    Platform.OS === "ios"
+      ? await getDefaultCalendarSource()
+      : { isLocalAccount: true, name: "Expo Calendar" };
+  const newCalendarID = await Calendar.createCalendarAsync({
+    title: "Break Time",
+    color: "blue",
+    entityType: Calendar.EntityTypes.EVENT,
+    sourceId: defaultCalendarSource.id,
+    source: defaultCalendarSource,
+    name: "internalCalendarName",
+    ownerAccount: "personal",
+    accessLevel: Calendar.CalendarAccessLevel.OWNER,
+  });
+  console.log(`Your new calendar ID is: ${newCalendarID}`);
+}
+
+async function getCalendar() {
+  const calendars = await Calendar.getCalendarsAsync(
+    Calendar.EntityTypes.EVENT
+  );
+  console.log("Here are all your calendars:");
+  console.log(calendars);
+  for (const calendar of calendars) {
+    // console.log(`Calendar with ID ${calendar.id} has name ${calendar.title}`);
+    if (calendar.title === "Break Time") {
+      console.log(`Found calendar with ID ${calendar.id}`);
+      return calendar.id;
+    }
+  }
+}
+
+async function createEvent() {
+  const calId = await getCalendar();
+  const caleve = await Calendar.createEventAsync(calId, {
+    title: "Break Time",
+    startDate: new Date(Date.now()),
+    endDate: new Date(Date.now() + 3600000),
+    alarms: [{ relativeOffset: -5 }],
+  });
+  console.log(`Your new event ID is: ${caleve}`);
+}
